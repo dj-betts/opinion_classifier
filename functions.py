44018babfc1a9045413842e912169b131c6e6d56
dj-betts <66078633+dj-betts@users.noreply.github.com>
2021-01-13 12:27:14 -0600

    cleaned functions

--- functions.py
+++ functions.py
@@ -11,23 +11,62 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+#combine print, type, text columns in the list. input is file, not dataframe.
+def df_printed(file):
+    print_cols = ['_id','print_page','pub_date','keywords','type_of_material','lead_paragraph','text']    
+    df = pd.read_csv(file, usecols=print_cols, index_col='_id', low_memory=False)
+    df = df[df.print_page.isna() == False]
+    df = df[~df.text.isna() == True]
+    return df
+
 # function to vectorize the type_of_material series into a y target vector.
 def vectorize_y_ser(ser):
     y = ser.copy()
     y.replace({'Op-Ed': 1,'News': 0}, inplace=True)
-    return y
+    print(y.head())
 
 #rate of correct predictions out of total predictions
 def metrics_(tn, fp, fn, tp):
     accuracy = (tp + tn) / (tn + fn + tp + fp)
-    print(f'accuracy = {accuracy}')
+    print(f'Accuracy: {round(accuracy, 2)}')
     recall = (tp) / (tp + fn)
-    print(f'recall = {recall}')
+    print(f'Recall: {round(recall, 2)}')
     precision = (tp) / (tp + fp)
-    print(f'precision = {precision}')
-    print(f'tn:{tn} fp:{fp} fn:{fn} tp:{tp}')
+    print(f'Precision: {round(precision, 2)}')
+    print(f'TN:{tn} FP:{fp} FN:{fn} TP:{tp}')
+    return (accuracy, recall, precision)
+
+#returns articles w/ 'United States Politics and Government' as a keyword
+def return_pol_gov(row):
+    list_dict = eval(row.keywords)
+    for (dic) in (list_dict):
+        if dic.get('value') == 'United States Politics and Government':
+            return True
+
+def filter_keyword(df, keyword='United States Politics and Government'):
+    df[keyword] = df.apply(return_pol_gov, axis=1)
+    df = df[df[keyword] == True]
+    return df
+
+#prints number of features, stop words and parameters for vectorizer
+def print_vector_params(vectorizer):
+    #features
+    feat_names = vectorizer.get_feature_names()
+    num_feat = len(feat_names)
+    
+    #stop words
+    stop_words = vectorizer.stop_words_
+    num_stop = len(stop_words)
+
+    params = vectorizer.get_params()
+    
+    print(f'Number of features: {num_feat}, Numbers of stop words: {num_stop}')
+      
+    for key, val in params.items():
+        print(f'{key}: {val}')
 
 #returns shape of news and oped
+#safe fig also included.
 def oped_v_news(df):
     df.type_of_material.hist()
     news = sum(df.type_of_material == "News")
@@ -48,18 +87,95 @@ def return_pol_gov(row):
     for (dic) in (list_dict):
         if dic.get('value') == 'United States Politics and Government':
             return True
-        
-def filter_keyword(df):
-    df['United States Politics and Government'] = df.apply(return_pol_gov, axis=1)    
-    df = df[df['United States Politics and Government'] == True]
-    oped_v_news(df)
+
+#filter articles by keyword reference. default: 'United States Politics and Government'  
+def filter_keyword(df, keyword='United States Politics and Government'):
+    df[keyword] = df.apply(return_pol_gov, axis=1)
+    df = df[df[keyword] == True]
     return df
 
-#function that removes string.punctuation w/out the '?'
-def punc_strip(document):
-    for char in document:
-        if char in '!"#$%&\'()*+—,-./:;<=>?@[\\]^_`{|}~”’':
-            document = document.replace(char, " ") 
-    return document
+#remove capitalized words such as first words of sentences. proper nouns.
+def remove_cap_words(row):
+    temp = row.split()
+    for i, word in enumerate(temp):
+        if word[0].isupper():
+            temp[i] = ""
+            
+    return " ".join(temp)
 
-stop_words = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','mr','ms','said']
+#function that removes string.punctuation w/out the '?'
+def punc_strip(doc):
+    for char in word_tokenize(doc):
+        if char in '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~—':
+            doc = doc.replace(char, " ")
+        if char == "’":
+            doc = doc.replace(char, "")
+    return doc
+
+#wordnet lemmatizer and lowercase words.
+def wordnet_tokenize(doc):
+    wordnet = WordNetLemmatizer()
+    return  [wordnet.lemmatize(word) for word in word_tokenize(doc.lower())]
+#split article and return quote
+
+#split document into article body and quotation
+def text_parse(document):
+    article = []
+    quote_list = []
+    
+    open_quote = "“"
+    close_quote = "”"
+    
+    close_split = document.split(close_quote)
+    
+    for string in close_split:
+
+        quote = string.split(open_quote)
+        article.append(quote.pop(0))
+        quote_list += quote
+    
+    article = " ".join(article)
+    quotation = " ".join(quote_list)
+        
+    return(article, quotation)
+
+#return list of quotes instaed of string to count how many times sources were quoted instead of volume of quotes.
+def return_quote_list(document):
+    article = []
+    quote_list = []
+    
+    open_quote = "“"
+    close_quote = "”"
+    
+    close_split = document.split(close_quote)
+    
+    for string in close_split:
+
+        quote = string.split(open_quote)
+        article.append(quote.pop(0))
+        quote_list.append(quote)
+    
+    article = " ".join(article)
+        
+    return(quote_list)
+
+#split article and return quote
+def return_article(document):
+    article = []
+    quote_list = []
+    
+    open_quote = "“"
+    close_quote = "”"
+    
+    close_split = document.split(close_quote)
+    
+    for string in close_split:
+
+        quote = string.split(open_quote)
+        article.append(quote.pop(0))
+        quote_list += quote
+    
+    article = " ".join(article)
+    quotation = " ".join(quote_list)
+    
+    return(article)
